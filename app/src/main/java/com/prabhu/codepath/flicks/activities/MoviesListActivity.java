c0bf84ef21678f09d0ed008628fdac7489240300
pbabu <prabhu.babu@elementum.com>
2016-07-20 06:47:16 -0700

    added basic array adapter support

--- app/src/main/java/com/prabhu/codepath/flicks/activities/MoviesListActivity.java
+++ app/src/main/java/com/prabhu/codepath/flicks/activities/MoviesListActivity.java
@@ -2,12 +2,15 @@ package com.prabhu.codepath.flicks.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 import com.prabhu.codepath.flicks.R;
+import com.prabhu.codepath.flicks.adapters.MoviesArrayAdapter;
 import com.prabhu.codepath.flicks.models.Movie;
 import com.prabhu.codepath.flicks.models.MovieDBApiResponse;
 import com.prabhu.codepath.flicks.rest.MovieDBClient;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -15,20 +18,24 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MoviesListActivity extends AppCompatActivity {
-
-    List<Movie> movies;
-
+    private List<Movie> movies;
+    private MoviesArrayAdapter moviesArrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies_list);
+        movies = new ArrayList<>();
+        moviesArrayAdapter = new MoviesArrayAdapter(this, movies);
+        ListView lvMovies = (ListView)findViewById(R.id.lvMovies);
+        lvMovies.setAdapter(moviesArrayAdapter);
         Call<MovieDBApiResponse> nowPlayingMoviesCall = MovieDBClient.getInstance()
                 .getMovieDBService()
                 .getNowPlayingMovies();
         nowPlayingMoviesCall.enqueue(new Callback<MovieDBApiResponse>() {
             @Override
             public void onResponse(Call<MovieDBApiResponse> call, Response<MovieDBApiResponse> response) {
-                movies = response.body().getResults();
+                movies.addAll(response.body().getResults());
+                moviesArrayAdapter.notifyDataSetChanged();
             }
 
             @Override
--- app/src/main/java/com/prabhu/codepath/flicks/adapters/MoviesArrayAdapter.java
+++ app/src/main/java/com/prabhu/codepath/flicks/adapters/MoviesArrayAdapter.java
@@ -1,15 +1,18 @@
 package com.prabhu.codepath.flicks.adapters;
 
 import android.content.Context;
-import android.graphics.Movie;
 import android.widget.ArrayAdapter;
+
+import com.prabhu.codepath.flicks.models.Movie;
+
+import java.util.List;
 
 /**
  * Created by pbabu on 7/20/16.
  */
 public class MoviesArrayAdapter extends ArrayAdapter<Movie> {
 
-    public MoviesArrayAdapter(Context context, int resource) {
-        super(context, resource);
+    public MoviesArrayAdapter(Context context, List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
     }
 }
