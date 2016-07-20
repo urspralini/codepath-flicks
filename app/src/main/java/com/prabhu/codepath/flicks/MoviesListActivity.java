e368492ddff2d72040990f0344f1783e5a8d58c8
pbabu <prabhu.babu@elementum.com>
2016-07-19 18:26:27 -0700

    First commit

---
+++ app/src/main/java/com/prabhu/codepath/flicks/MoviesListActivity.java
@@ -0,0 +1,13 @@
+package com.prabhu.codepath.flicks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies_list);
+    }
+}
